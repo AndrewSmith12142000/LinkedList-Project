7da1867302951bf22ead447f7c112bb2f33e309d
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-10-09 21:17:48 -0500

    Created public methods

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,32 @@
 /*
  * your comment header here
  */
+#ifndef LINKED_LIST_h
+#define LINKED_LIST_h
+
+# include "data.h"
+# include <iomanip>
+# include <string>
+# include <stdlib.h>
+# include <iostream>
+# include <new>
+
+class LinkedList
+{
+public:
+    LinkedList();
+    ~LinkedList();
+  
+    bool addNode(int, string*);
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    void printList(bool = false);
+    int getCount();
+    bool clearList();
+    bool exists(int);
+
+private:
+	
+};
+
+# endif
