f9439af602ddc62bb92a5dd966fd8fd401dd8ea4
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-10-09 22:42:46 -0500

    Added addNode function

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,3 +20,33 @@ bool LinkedList::addHead(int id, string* data)
     head->prev = nullptr;
     return true;
 }
+
+bool LinkedList::addNode(int id, string* data)
+{
+    bool success = false;
+
+    if (id > 0 && !head)
+    {
+        success = this->addHead(id, data);
+    }
+    else if (id > 0 && head)
+    {
+        Node* current = head;
+        Node* newNode = new Node;
+        newNode->data.id = id;
+        newNode->data.data = *data;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+
+        while (current->next && id > current->data.id)
+        {
+            current = current->next;
+        }
+
+        if (id != current->data.id)
+        {
+            success = nodeCouple(success, id, data, current, newNode);
+        }
+    }
+    return success;
+}
