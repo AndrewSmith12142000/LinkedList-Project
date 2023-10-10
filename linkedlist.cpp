985de3c4f818a5ad1eaac3ce40019b6af5be975a
AndrewSmith12142000 <ungee.smith@gmail.com>
2023-10-09 22:28:47 -0500

    Added addHead function

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -10,3 +10,13 @@ LinkedList::~LinkedList()
 {
     this->clearList();
 }
+
+bool LinkedList::addHead(int id, string* data)
+{
+    head = new Node;
+    head->data.id = id;
+    head->data.data = *data;
+    head->next = nullptr;
+    head->prev = nullptr;
+    return true;
+}
